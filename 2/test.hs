edb8d57ba82a9e8a92ba673999c52e97a8edea0a
Koji Onishi <fursich0@gmail.com>
2020-08-15 12:12:16 +0900

    practice with textbook samples

---
+++ 2/test.hs
@@ -0,0 +1,11 @@
+-- no main function included. this program is for gchi use
+double x = x + x
+quadruple x = double(double x)
+
+factorial n = product [1..n]
+average ns = sum ns `div` length ns
+
+{-
+  this part has to be considied as
+  lines of comments, and it is.
+-}
